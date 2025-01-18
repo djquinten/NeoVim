d7c654b6dc03d601985dd561a19752f36a171d25
djquinten <quinten1vanderlaan@outlook.com>
2025-01-18 21:05:30 +0100

    added bufferline

---
+++ lua/quinten/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+    "akinsho/nvim-bufferline.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        options = {
+            mode = "tabs",
+            separator_style = "slope",
+        }
+    }
+}
